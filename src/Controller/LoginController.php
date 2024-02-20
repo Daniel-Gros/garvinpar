659a72aec8d3d304f455fd2a7c70f9efe4d251bb
Daniel Gros <danielgrosbgns@gmail.com>
2024-02-20 20:48:05 +0100

    clean the login controller

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\FormLogin;
-use App\Form\FormLogAdmin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class LoginController extends AbstractController
 {
   #[Route('/login', name: 'app_login')]
-  public function formLogin(): Response
+  public function login(): Response
   {
     return $this->render('form/formlogin.html.twig', [
       'controller_name' => 'LoginController',
